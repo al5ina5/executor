44e62ab512899a58ab58df2537ec42da16755c0a
Sebastian Alsina <alsinas@me.com>
2023-10-11 10:54:53 -0400

    igorers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    compiler: {
+        removeConsole: process.env.NODE_ENV === "production"
+    },
+    reactStrictMode: false,
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
+}
 
 module.exports = nextConfig
